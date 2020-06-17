59bb7afb96f836efbaa27a69ab323a789f84bc0a
gorchicity <gorchicity@yahoo.com>
2020-06-17 17:23:40 +0430

    update get features method

---
+++ src/DataCollector/Models/Map/GetFeaturesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mah.DataCollector.Web.Models.Map
+{
+    public class GetFeaturesViewModel
+    {
+        public IEnumerable<GetFeaturesClassViewModel> LayerNames { get; set; }
+        public int MapWidth { get; set; }
+
+    }
+
+    public class GetFeaturesClassViewModel
+    {
+        public string LayerName { get; set; }
+        public string UniqueField { get; set; }
+        public double[] MapExtent { get; set; }
+        public int SRID { get; set; }
+    }
+}
